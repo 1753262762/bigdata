b36cb6af7705e3e449654750219a62f98c3fe887
elma <1753262762@qq.com>
2025-08-19 11:27:14 +0800

    测试罢了

--- README.md
+++ README.md
@@ -1,10 +1,5 @@
-# bigdata  
-高精度计算器  
--------  
-***目的：为了计算大数据***  
------  
-编程语言：C++  
-编程环境：Microsoft Visual Studio  
+编程语言:C++  
+编程环境:Microsoft Visual Studio  
 使用类与对象实现 [代码](https://github.com/1753262762/bigdata/blob/master/main.md)  
 ------
 [目前已完工](https://github.com/1753262762/bigdata/tree/master/%E9%A1%B9%E7%9B%AE)  
--- 项目/main.cpp
+++ 项目/main.cpp
@@ -1,27 +1,31 @@
 /*
 	main
 */
-#include"_MES_W_T.h"
-#include"_EES_W_T.h"
+#include "_MES_W_T.h"
+#include "_EES_W_T.h"
 using namespace std;
-int main() {
-	printf("计算器高精\n");
-	printf("作者：weizi"); 
+int main()
+{
 	string s;
-	while(cin>>s) {
-		BigNum a,b,sum;
-		cin>>a>>b;
-		if(s=="+") {
-			sum=a+b;
-		} else if(s=="-") {
-			sum=a-b;
+	while (cin >> s)
+	{
+		BigNum a, b, sum;
+		cin >> a >> b;
+		if (s == "+")
+		{
+			sum = a + b;
 		}
-		else if(s=="*"){
-			sum=a*b;
+		else if (s == "-")
+		{
+			sum = a - b;
 		}
-//		else if(s=="/"){
-//			sum=a/b;
-//		}
+		else if (s == "*")
+		{
+			sum = a * b;
+		}
+		//		else if(s=="/"){
+		//			sum=a/b;
+		//		}
 		sum.print();
 	}
 	return 0;
